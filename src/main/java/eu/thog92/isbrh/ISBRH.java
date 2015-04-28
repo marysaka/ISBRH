4ca97809d0c33ce03c6bf746fc28e099e7a3bd88
Thog <thog92@hotmail.fr>
2015-04-28 23:00:19 +0200

    Use clientSideOnly parameter in @Mod

--- src/main/java/eu/thog92/isbrh/ISBRH.java
+++ src/main/java/eu/thog92/isbrh/ISBRH.java
@@ -20,7 +20,6 @@ import net.minecraft.entity.item.EntityItemFrame;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
@@ -33,77 +32,83 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = "isbrhcore", name = "ISBRH", acceptedMinecraftVersions = "[1.8]")
-public class ISBRH {
-    public static final Block test = new BlockExample().setUnlocalizedName("test");
+@Mod(modid = "isbrhcore", name = "ISBRH", clientSideOnly = true, acceptedMinecraftVersions = "[1.8]")
+public class ISBRH
+{
+    public static final Block test = new BlockExample()
+            .setUnlocalizedName("test");
     public static final ResourceLocation textureLocation = new ResourceLocation(
             "isbrhcore:blocks/test");
     public static int testId;
-    private static boolean exempleEnabled = Boolean.parseBoolean(System.getProperty("isbrhCore.enableExemple", "false"));
+    private static boolean exempleEnabled = Boolean.parseBoolean(System
+            .getProperty("isbrhCore.enableExemple", "false"));
     private Logger logger;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
+    public void preInit(FMLPreInitializationEvent event)
+    {
         logger = event.getModLog();
         MinecraftForge.EVENT_BUS.register(this);
-        
 
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event) {
-
-        if(FMLCommonHandler.instance().getSide().isServer())
+    public void init(FMLInitializationEvent event)
+    {
+        if (exempleEnabled)
         {
-            logger.error("WHY AM I HERE? I'm a client side only mod!");
-            return;
-        }
-        else if (exempleEnabled) {
             logger.info("Registering ISBRH Block Exemple...");
             testId = RenderRegistry.getNextAvailableRenderId();
             GameRegistry.registerBlock(test, "test");
             RenderRegistry.registerBlockHandler(new RenderExample());
             RenderRegistry.registerTextureHandler((ITextureHandler) test);
         }
-        
+
         Minecraft mc = Minecraft.getMinecraft();
-        
+
         // Prerequisite
-        ModelManager modelManager = ObfuscationReflectionHelper.getPrivateValue(Minecraft.class, mc, "aL", "field_175617_aL", "modelManager");
+        ModelManager modelManager = ObfuscationReflectionHelper
+                .getPrivateValue(Minecraft.class, mc, "aL", "field_175617_aL",
+                        "modelManager");
         RenderManager renderManager = mc.getRenderManager();
-        IReloadableResourceManager resourceManager = ((IReloadableResourceManager) mc.getResourceManager());
-        
+        IReloadableResourceManager resourceManager = ((IReloadableResourceManager) mc
+                .getResourceManager());
+
         // Render Item Hook
-        RenderItem item = new RenderItemISBRH(mc.getTextureManager(), modelManager);
-        ObfuscationReflectionHelper.setPrivateValue(Minecraft.class, mc, item, "X", "field_175621_X", "renderItem");
-        ObfuscationReflectionHelper.setPrivateValue(Minecraft.class, mc, new ItemRenderer(mc), "Y", "field_175620_Y", "itemRenderer");
+        RenderItem item = new RenderItemISBRH(mc.getTextureManager(),
+                modelManager);
+        ObfuscationReflectionHelper.setPrivateValue(Minecraft.class, mc, item,
+                "X", "field_175621_X", "renderItem");
+        ObfuscationReflectionHelper.setPrivateValue(Minecraft.class, mc,
+                new ItemRenderer(mc), "Y", "field_175620_Y", "itemRenderer");
         renderManager.entityRenderMap.remove(EntityItem.class);
-        renderManager.entityRenderMap.put(EntityItem.class, new RenderEntityItem(renderManager, item));
+        renderManager.entityRenderMap.put(EntityItem.class,
+                new RenderEntityItem(renderManager, item));
         renderManager.entityRenderMap.remove(EntityItemFrame.class);
-        renderManager.entityRenderMap.put(EntityItemFrame.class, new RenderItemFrame(renderManager, item));
+        renderManager.entityRenderMap.put(EntityItemFrame.class,
+                new RenderItemFrame(renderManager, item));
         mc.entityRenderer = new EntityRenderer(mc, resourceManager);
 
-        
         // Render Block Dispatcher Hook
-        BlockRendererDispatcher rendererDispatcher = new ISBRHDispatcher(modelManager.getBlockModelShapes(), mc.gameSettings);
-        ObfuscationReflectionHelper.setPrivateValue(Minecraft.class, mc, rendererDispatcher, "aM", "field_175618_aM", "blockRenderDispatcher");
-        
-        
+        BlockRendererDispatcher rendererDispatcher = new ISBRHDispatcher(
+                modelManager.getBlockModelShapes(), mc.gameSettings);
+        ObfuscationReflectionHelper.setPrivateValue(Minecraft.class, mc,
+                rendererDispatcher, "aM", "field_175618_aM",
+                "blockRenderDispatcher");
+
         // Register Reload Listeners
         resourceManager.registerReloadListener(rendererDispatcher);
         resourceManager.registerReloadListener(item);
         resourceManager.registerReloadListener(mc.entityRenderer);
-        
-
-        
-        
 
     }
 
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
-    public void onTextureStitch(TextureStitchEvent.Pre event) {
-        if (event.map == Minecraft.getMinecraft().getTextureMapBlocks()) {
+    public void onTextureStitch(TextureStitchEvent.Pre event)
+    {
+        if (event.map == Minecraft.getMinecraft().getTextureMapBlocks())
+        {
             RenderRegistry.instance().injectTexture(event.map);
         }
     }
