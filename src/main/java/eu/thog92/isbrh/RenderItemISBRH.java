2d64ef977ba4752eb01f9cd8ca16d56c7a4ce877
Thomas GUILLEMARD <tguillem@student.42.fr>
2016-01-11 16:06:45 +0100

    WIP RenderItem wrapping

--- src/main/java/eu/thog92/isbrh/RenderItemISBRH.java
+++ src/main/java/eu/thog92/isbrh/RenderItemISBRH.java
@@ -8,6 +8,7 @@ import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformT
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.client.resources.model.IBakedModel;
 import net.minecraft.client.resources.model.ModelManager;
 import net.minecraft.item.ItemBlock;
@@ -19,13 +20,14 @@ public class RenderItemISBRH extends RenderItem
 {
 
     protected TextureManager textureManager;
+    private final RenderItem wrapped;
 
     public RenderItemISBRH(TextureManager textureManager,
             ModelManager modelManager, RenderItem renderItem)
     {
         super(textureManager, modelManager);
         this.textureManager = textureManager;
-
+        this.wrapped = renderItem;
         try
         {
             Field mesher = ReflectionHelper.findField(RenderItem.class, "m", "field_175059_m", "itemModelMesher");
@@ -40,87 +42,97 @@ public class RenderItemISBRH extends RenderItem
         }
     }
 
-    @Override
-    public void renderItem(ItemStack stack, IBakedModel model)
+    private boolean isISBRH(ItemStack stack)
     {
-        this.renderItem(stack, model, TransformType.NONE);
+        return (((stack.getItem() instanceof ItemBlock))
+            && (((ItemBlock) stack.getItem()).getBlock()
+            .getRenderType() > 4));
     }
 
     @Override
-    protected void renderItemModelTransform(ItemStack stack, IBakedModel model,
+    public void renderItem(ItemStack stack, IBakedModel model)
+    {
+        if (isISBRH(stack))
+            this.renderItem(stack, TransformType.NONE);
+        else
+            wrapped.renderItem(stack, model);
+    }
+
+    @Override
+    public void renderItemModelTransform(ItemStack stack, IBakedModel model,
             ItemCameraTransforms.TransformType cameraTransformType)
     {
-        this.textureManager.bindTexture(TextureMap.locationBlocksTexture);
-        this.textureManager.getTexture(TextureMap.locationBlocksTexture)
-                .setBlurMipmap(false, false);
-        this.preTransform(stack);
-        GlStateManager.enableRescaleNormal();
-        GlStateManager.alphaFunc(516, 0.1F);
-        GlStateManager.enableBlend();
-        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-        GlStateManager.pushMatrix();
-
-        model = net.minecraftforge.client.ForgeHooksClient
-                .handleCameraTransforms(model, cameraTransformType);
-
-        renderItem(stack, model, cameraTransformType);
-        GlStateManager.popMatrix();
-        GlStateManager.disableRescaleNormal();
-        GlStateManager.disableBlend();
-        this.textureManager.bindTexture(TextureMap.locationBlocksTexture);
-        this.textureManager.getTexture(TextureMap.locationBlocksTexture)
-                .restoreLastBlurMipmap();
+        if (isISBRH(stack))
+        {
+            this.textureManager.bindTexture(TextureMap.locationBlocksTexture);
+            this.textureManager.getTexture(TextureMap.locationBlocksTexture)
+                    .setBlurMipmap(false, false);
+            this.preTransform(stack);
+            GlStateManager.enableRescaleNormal();
+            GlStateManager.alphaFunc(516, 0.1F);
+            GlStateManager.enableBlend();
+            GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+            GlStateManager.pushMatrix();
+            renderItem(stack, cameraTransformType);
+            GlStateManager.popMatrix();
+            GlStateManager.disableRescaleNormal();
+            GlStateManager.disableBlend();
+            this.textureManager.bindTexture(TextureMap.locationBlocksTexture);
+            this.textureManager.getTexture(TextureMap.locationBlocksTexture)
+                    .restoreLastBlurMipmap();
+        } else
+            wrapped.renderItemModelTransform(stack, model, cameraTransformType);
     }
 
     @Override
     public void renderItemIntoGUI(ItemStack stack, int x, int y)
     {
-        IBakedModel ibakedmodel = this.getItemModelMesher().getItemModel(stack);
-        GlStateManager.pushMatrix();
-        this.textureManager.bindTexture(TextureMap.locationBlocksTexture);
-        this.textureManager.getTexture(TextureMap.locationBlocksTexture)
-                .setBlurMipmap(false, false);
-        GlStateManager.enableRescaleNormal();
-        GlStateManager.enableAlpha();
-        GlStateManager.alphaFunc(516, 0.1F);
-        GlStateManager.enableBlend();
-        GlStateManager.blendFunc(770, 771);
-        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-        setupGuiTransform(x, y, ibakedmodel.isGui3d());
-        applyTransform(ibakedmodel.getItemCameraTransforms().gui);
-        renderItem(stack, ibakedmodel, ItemCameraTransforms.TransformType.GUI);
-        GlStateManager.disableAlpha();
-        GlStateManager.disableRescaleNormal();
-        GlStateManager.disableLighting();
-        GlStateManager.popMatrix();
-        this.textureManager.bindTexture(TextureMap.locationBlocksTexture);
-        this.textureManager.getTexture(TextureMap.locationBlocksTexture)
-                .restoreLastBlurMipmap();
+        if (isISBRH(stack))
+        {
+            IBakedModel ibakedmodel = this.getItemModelMesher().getItemModel(stack);
+            GlStateManager.pushMatrix();
+            this.textureManager.bindTexture(TextureMap.locationBlocksTexture);
+            this.textureManager.getTexture(TextureMap.locationBlocksTexture)
+                    .setBlurMipmap(false, false);
+            GlStateManager.enableRescaleNormal();
+            GlStateManager.enableAlpha();
+            GlStateManager.alphaFunc(516, 0.1F);
+            GlStateManager.enableBlend();
+            GlStateManager.blendFunc(770, 771);
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+            setupGuiTransform(x, y, ibakedmodel.isGui3d());
+            applyTransform(ibakedmodel.getItemCameraTransforms().gui);
+            renderItem(stack, ItemCameraTransforms.TransformType.GUI);
+            GlStateManager.disableAlpha();
+            GlStateManager.disableRescaleNormal();
+            GlStateManager.disableLighting();
+            GlStateManager.popMatrix();
+            this.textureManager.bindTexture(TextureMap.locationBlocksTexture);
+            this.textureManager.getTexture(TextureMap.locationBlocksTexture)
+                    .restoreLastBlurMipmap();
+        }
+        else
+        {
+            wrapped.renderItemIntoGUI(stack, x, y);
+        }
     }
 
-    public void renderItem(ItemStack paramItemStack,
-            IBakedModel paramIBakedModel,
-            ItemCameraTransforms.TransformType paramTransformType)
+    private void renderItem(ItemStack paramItemStack, ItemCameraTransforms.TransformType paramTransformType)
     {
-        if (((paramItemStack.getItem() instanceof ItemBlock))
-                && (((ItemBlock) paramItemStack.getItem()).getBlock()
-                        .getRenderType() > 4))
-            RenderRegistry.instance().renderInventoryBlock(paramItemStack,
-                    paramTransformType);
-        else
-            super.renderItem(paramItemStack, paramIBakedModel);
+        RenderRegistry.instance().renderInventoryBlock(paramItemStack, paramTransformType);
     }
 
     @Override
     public boolean shouldRenderItemIn3D(ItemStack stack)
     {
-        IBakedModel ibakedmodel = this.getItemModelMesher().getItemModel(stack);
-        if (ibakedmodel == null
-                || ibakedmodel == this.getItemModelMesher().getModelManager()
-                        .getMissingModel())
+        if (isISBRH(stack))
             return RenderRegistry.instance().shouldRender3DInInventory(stack);
 
-        return ibakedmodel.isGui3d();
+        return wrapped.shouldRenderItemIn3D(stack);
     }
 
+    @Override
+    public void onResourceManagerReload(IResourceManager resourceManager) {
+        wrapped.onResourceManagerReload(resourceManager);
+    }
 }
