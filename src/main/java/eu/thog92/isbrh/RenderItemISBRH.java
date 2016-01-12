6dbc135fd20bae8dcc2acc067f9408888c338109
Thog <thog92@hotmail.fr>
2016-01-12 12:00:40 +0100

    (Final?) version for 1.8

--- src/main/java/eu/thog92/isbrh/RenderItemISBRH.java
+++ src/main/java/eu/thog92/isbrh/RenderItemISBRH.java
@@ -1,16 +1,25 @@
 package eu.thog92.isbrh;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
+import net.minecraft.block.Block;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.ItemModelMesher;
+import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
+import net.minecraft.client.renderer.block.model.ItemTransformVec3f;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.client.resources.model.IBakedModel;
 import net.minecraft.client.resources.model.ModelManager;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
@@ -40,6 +49,114 @@ public class RenderItemISBRH extends RenderItem
         {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void registerBlock(Block blk, String identifier)
+    {
+        wrapped.registerBlock(blk, identifier);
+    }
+
+    @Override
+    public void registerItem(Item itm, String identifier)
+    {
+        wrapped.registerItem(itm, identifier);
+    }
+
+    @Override
+    public void registerBlock(Block blk, int subType, String identifier)
+    {
+        wrapped.registerBlock(blk, subType, identifier);
+    }
+
+    @Override
+    public void registerItem(Item itm, int subType, String identifier)
+    {
+        wrapped.registerItem(itm, subType, identifier);
+    }
+
+    @Override
+    public ItemModelMesher getItemModelMesher()
+    {
+        return wrapped.getItemModelMesher();
+    }
+
+    @Override
+    public void setupGuiTransform(int xPosition, int yPosition, boolean isGui3d)
+    {
+        wrapped.setupGuiTransform(xPosition, yPosition, isGui3d);
+    }
+
+    @Override
+    public void renderQuads(WorldRenderer renderer, List quads, int color, ItemStack stack)
+    {
+        wrapped.renderQuads(renderer, quads, color, stack);
+    }
+
+    @Override
+    public void renderQuad(WorldRenderer renderer, BakedQuad quad, int color)
+    {
+        wrapped.renderQuad(renderer, quad, color);
+    }
+
+    @Override
+    public void renderEffect(IBakedModel model)
+    {
+        wrapped.renderEffect(model);
+    }
+
+    @Override
+    public void renderItemModel(ItemStack stack)
+    {
+        wrapped.renderItemModel(stack);
+    }
+
+    @Override
+    public void renderModel(IBakedModel model, int color)
+    {
+        wrapped.renderModel(model, color);
+    }
+
+    @Override
+    public void renderModel(IBakedModel model, int color, ItemStack stack)
+    {
+        wrapped.renderModel(model, color, stack);
+    }
+
+    @Override
+    public void renderModel(IBakedModel model, ItemStack stack)
+    {
+        wrapped.renderModel(model, stack);
+    }
+
+    @Override
+    public void putQuadNormal(WorldRenderer renderer, BakedQuad quad)
+    {
+        wrapped.putQuadNormal(renderer, quad);
+    }
+
+    @Override
+    public void applyTransform(ItemTransformVec3f transform)
+    {
+        wrapped.applyTransform(transform);
+    }
+
+    @Override
+    public void preTransform(ItemStack stack)
+    {
+        wrapped.preTransform(stack);
+    }
+
+    @Override
+    public void drawRect(WorldRenderer renderer, int x, int y, int width, int height, int color)
+    {
+        wrapped.drawRect(renderer, x, y, width, height, color);
+    }
+
+    @Override
+    public void func_175039_a(boolean p_175039_1_)
+    {
+        wrapped.func_175039_a(p_175039_1_);
     }
 
     private boolean isISBRH(ItemStack stack)
@@ -134,5 +251,12 @@ public class RenderItemISBRH extends RenderItem
     @Override
     public void onResourceManagerReload(IResourceManager resourceManager) {
         wrapped.onResourceManagerReload(resourceManager);
+    }
+
+
+    @Override
+    public void registerItems()
+    {
+
     }
 }
